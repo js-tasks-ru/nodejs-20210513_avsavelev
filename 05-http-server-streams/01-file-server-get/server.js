2278f273921a25cd622084bd114f462d5322ade7
Alexandr Savelev <avsavelev@gmail.com>
2021-05-30 19:12:39 +0300

    My implementation

--- 05-http-server-streams/01-file-server-get/server.js
+++ 05-http-server-streams/01-file-server-get/server.js
@@ -1,17 +1,34 @@
 const url = require('url');
 const http = require('http');
 const path = require('path');
+const fs = require('fs');
 
 const server = new http.Server();
 
 server.on('request', (req, res) => {
   const pathname = url.parse(req.url).pathname.slice(1);
-
   const filepath = path.join(__dirname, 'files', pathname);
-
+  const isIncludesDir = pathname.includes('/');
+  console.log(pathname);
   switch (req.method) {
     case 'GET':
-
+      if (isIncludesDir) {
+        res.statusCode = 400;
+        return res.end('Not found!');
+      } else {
+        const streamRead = fs.createReadStream(filepath);
+        streamRead.on('error', (err) => {
+          if (err.code === 'ENOENT') {
+            res.statusCode = 404;
+            res.end('File not found');
+          } else {
+            res.statusCode = 500;
+            res.end('Internal Server Error');
+          }
+          res.end('Not found!');
+        });
+        streamRead.pipe(res);
+      }
       break;
 
     default:
