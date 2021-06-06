d417f1243fed786bcf434e2abde4cc67ab52b686
Alexandr Savelev <avsavelev@gmail.com>
2021-06-06 19:11:30 +0300

    Test on windows aborted

--- 05-http-server-streams/02-file-server-post/server.js
+++ 05-http-server-streams/02-file-server-post/server.js
@@ -1,5 +1,7 @@
 const url = require('url');
 const http = require('http');
+const fs = require('fs');
+const LimitSizeStream = require('./LimitSizeStream');
 const path = require('path');
 
 const server = new http.Server();
@@ -11,13 +13,55 @@ server.on('request', (req, res) => {
 
   switch (req.method) {
     case 'POST':
+      if (pathname.includes('/') || pathname.includes('..')) {
+        res.statusCode = 400;
+        return res.end('Nested paths are not allowed');
+      };
+
+      const limitedStream = new LimitSizeStream({limit: 1048576});
+      const writeStream = fs.createWriteStream(filepath, {flags: 'wx'});
+
+      let isAborted = false;
+
+      function destroyWriteStream() {
+        isAborted = true;
+        writeStream.destroy();
+      }
+
+      req.pipe(limitedStream).pipe(writeStream);
+
+      writeStream.on('error', (error) => {
+        if (error.code === 'EEXIST') {
+          res.statusCode = 409;
+          res.end('File is alredy exist');
+        }
+      });
+
+      writeStream.on('close', () => {
+        if (isAborted) {
+          fs.unlink(filepath, () => {});
+        }
+      });
+
+      writeStream.on('finish', () => {
+        res.statusCode = 201;
+        res.end('It\'s all right');
+      });
+
+      limitedStream.on('error', () => {
+        res.statusCode = 413;
+        res.end('Large post request body');
+        destroyWriteStream();
+      });
+
+      req.on('aborted', destroyWriteStream);
 
       break;
 
     default:
       res.statusCode = 501;
       res.end('Not implemented');
-  }
+  };
 });
 
 module.exports = server;
