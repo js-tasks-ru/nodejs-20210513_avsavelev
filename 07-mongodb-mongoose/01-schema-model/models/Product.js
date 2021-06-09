14220bb8f41fb2a0633c0bf3e7f9d23243bef9e4
Alexandr Savelev <avsavelev@gmail.com>
2021-06-09 11:10:40 +0300

    01-schema-model

--- 07-mongodb-mongoose/01-schema-model/models/Product.js
+++ 07-mongodb-mongoose/01-schema-model/models/Product.js
@@ -1,7 +1,32 @@
 const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+const objectId = Schema.Types.ObjectId;
 const connection = require('../libs/connection');
+// const types = mongoose.Schema.Types;
 
-const productSchema = new mongoose.Schema({
+const productSchema = new Schema({
+  title: {
+    type: String,
+    required: true,
+  },
+  description: {
+    type: String,
+    required: true,
+  },
+  price: {
+    type: Number,
+    required: true,
+  },
+  category: {
+    type: objectId,
+    ref: 'Category',
+    required: true,
+  },
+  subcategory: {
+    type: objectId,
+    required: true,
+  },
+  images: [String],
 });
 
 module.exports = connection.model('Product', productSchema);
