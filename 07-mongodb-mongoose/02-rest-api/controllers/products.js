1fc81b73763ee8a0714055def7e6a7fa1d78ced5
Alexandr Savelev <avsavelev@gmail.com>
2021-06-14 23:12:49 +0300

    02-rest-api

--- 07-mongodb-mongoose/02-rest-api/controllers/products.js
+++ 07-mongodb-mongoose/02-rest-api/controllers/products.js
@@ -1,12 +1,43 @@
+const Product = require('../models/Product');
+const {ObjectID} = require('mongodb');
+
 module.exports.productsBySubcategory = async function productsBySubcategory(ctx, next) {
-  ctx.body = {};
+  const subcategoryId = ctx.query.subcategory;
+
+  if (subcategoryId) {
+    try {
+      // eslint-disable-next-line new-cap
+      const productsBySubcategory = await Product.find({subcategory: ObjectID(subcategoryId)});
+
+      ctx.body = {products: productsBySubcategory};
+    } catch (e) {
+      ctx.res.statusCode = 400;
+      ctx.res.end();
+    }
+  } else {
+    await next();
+  }
 };
 
 module.exports.productList = async function productList(ctx, next) {
-  ctx.body = {};
+  const products = await Product.find().limit(20);
+
+  ctx.body = {products};
 };
 
 module.exports.productById = async function productById(ctx, next) {
-  ctx.body = {};
-};
+  const productId = ctx.params.id;
 
+  if (!ObjectID.isValid(productId)) {
+    ctx.throw(400, 'invalid product id');
+  } else {
+    // eslint-disable-next-line new-cap
+    const product = await Product.findById(ObjectID(productId));
+
+    if (product) {
+      ctx.body = {product: product};
+    } else {
+      ctx.throw(404, `no product with ${productId} id`);
+    }
+  }
+};
