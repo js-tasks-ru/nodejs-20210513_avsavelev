1f5fb53f73894fa272731d3f56edf8bc964ca161
Alexandr Savelev <avsavelev@gmail.com>
2021-06-23 19:05:51 +0300

    09-email-order

--- 09-email/02-order/controllers/orders.js
+++ 09-email/02-order/controllers/orders.js
@@ -1,8 +1,31 @@
 const Order = require('../models/Order');
+const Product = require('../models/Product');
 const sendMail = require('../libs/sendMail');
+const mapOrder = require('../mappers/order');
 
 module.exports.checkout = async function checkout(ctx, next) {
+  const user = ctx.user.id;
+  const {product, phone, address} = ctx.request.body;
+  const order = await Order.create({
+    user,
+    product,
+    phone,
+    address,
+  });
+
+  const productData = await Product.findById(product);
+
+  await sendMail({
+    to: ctx.user.email,
+    subject: 'Подтверждение регистрации',
+    locals: {id: order.id, product: productData},
+    template: 'order-confirmation',
+  });
+
+  ctx.body = {order: order.id};
 };
 
 module.exports.getOrdersList = async function ordersList(ctx, next) {
+  const orders = await Order.find({user: ctx.user}).populate('product');
+  ctx.body = {orders: orders.map(mapOrder)};
 };
