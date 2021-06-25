4551c6b559a2dd985f6becd9ec65f05579055545
Alexandr Savelev <avsavelev@gmail.com>
2021-06-25 08:33:08 +0300

    chat

--- 10-websockets/01-chat/socket.js
+++ 10-websockets/01-chat/socket.js
@@ -2,37 +2,36 @@ const socketIO = require('socket.io');
 
 const Session = require('./models/Session');
 const Message = require('./models/Message');
-
 function socket(server) {
   const io = socketIO(server);
 
   io.use(async function(socket, next) {
-    const {token} = socket.handshake.query;
-
+    const token = socket.handshake.query.token;
     if (!token) {
       return next(new Error('anonymous sessions are not allowed'));
     }
-
-    const session = await Session.findOne({Session}).populate('user');
-
+    const session = await Session.findOne({token}).populate('user');
     if (!session) {
       return next(new Error('wrong or expired session token'));
     }
-
+    session.lastVisit = new Date();
+    await session.save();
     socket.user = session.user;
     next();
   });
 
   io.on('connection', function(socket) {
     socket.on('message', async (msg) => {
-      const user = socket.user;
-
-      await Message.create({
-        user: user.displayName,
-        chat: user.id,
-        text: msg,
-        date: new Date(),
-      });
+      try {
+        await Message.create({
+          date: new Date(),
+          text: msg,
+          chat: socket.user.id,
+          user: socket.user.displayName,
+        });
+      } catch (err) {
+        return next(new Error('validation error'));
+      }
     });
   });
 
@@ -40,4 +39,3 @@ function socket(server) {
 }
 
 module.exports = socket;
-
